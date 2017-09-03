6a8c40eec75a2d59aa6cfe2fc1f55477be8fa258
kayquer <kayquerocha@gmail.com>
2017-09-03 15:30:26 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
+#include <locale.h>
 
-int x,y,t,rx,ry,b,ac=1,bc=1,barcos,contador,player[32][32],cpu[32][32],acertos_player,acertos_cpu,erros_player,erros_cpu,barcos_player=1,barcos_cpu=1;
-char a;
+
+int x,xp,y,t,rx,ry,b,bp,c,ac=1,bc=1,barcos,contador,player[32][32],cpu[32][32],acertos_player,acertos_cpu,erros_player,erros_cpu,barcos_player=1,barcos_cpu=1,asc,printasc,ascp,printascp,ok;
+char a,aa[2],bb[2],ap,app[1],bpp[1],escolher;
 
 
 
@@ -28,7 +31,6 @@ void criaguia(int t){
 void criatabuleiro(int t){
   for(x=1;x<=t;x++)
     for(y=1;y<=t;y++){
-
       player[x][y]='~';
       player[x][y]='~';
       cpu[x][y]='~';
@@ -36,27 +38,76 @@ void criatabuleiro(int t){
     }
 
 }
+void criabarco_player(int t,char escolher){
+
+
+  if(escolher == 's'){
+    while(xp<t){
+        desenhatabuleiro(t);
+
+        printf("Digite as Posições do Barcos %d de %d:\n",xp+1,t);
+        fgets(app,2,stdin);
+
+        fgets(bpp,2,stdin);
+
+        if(isalpha(app[0]) && isdigit(bpp[0])){
+          ap = app[0];
+          bp = atoi(bpp);
+        }
+
+    while((app[0] = getchar()) != '\n' && app[0] != EOF);
+    if(!(isalpha(ap) || isdigit(bp))){
+      printf("Posições Incorretas! \n Digite Novamente Nas Posições Corretas\n");
+    }
+      if(isalpha(ap) || isdigit(bp)){
+
+        if(ap >= 'a' && ap <= 'z'){
+          ascp = 96;
+          printascp = 32;
+        }
+        if(ap >= 'A' && ap <= 'Z'){
+          asc = 64;
+          printascp = 0;
+        }
+        if(player[ap-ascp][bp]=='o'){
+          printf("Você já escolheu Esta Posição ou Ela é Inválida\n");
+        }
+
+        if(ap-ascp>t || bp > t){
+          printf("Posição Fora do Tabuleiro\n");
+        }
+        if(player[ap-ascp][bp] == '~'){
+          printf("Você Escolheu [%c] [%d] \n",ap-printascp,bp);
+          player[ap-ascp][bp] = 'o';
+          xp+=1 ;
+        }
+      }
+    }
+  }
+
+}
 
 void criabarcoaleat(int t){
-  srand(time(NULL));
-  do{
+  if(escolher=='n'){
+      srand(time(NULL));
+      do{
         rx = rand()%t;
         ry = rand()%t;
         if (rx != 0 && ry !=0 && player[rx][ry] !='o'){
-        player[rx][ry]='o';
-        contador ++;
+          player[rx][ry]='o';
+          contador ++;
         }
-  }while(contador < t);
-    contador = 0;
-
-  do{
-        rx = rand()%t;
-        ry = rand()%t;
-        if (rx != 0 && ry !=0 && cpu[rx][ry] !='o'){
-        cpu[rx][ry]='o';
-        contador ++;
-        }
-  }while(contador < t);
+      }while(contador < t);
+        contador = 0;
+      }
+      do{
+          rx = rand()%t;
+          ry = rand()%t;
+          if (rx != 0 && ry !=0 && cpu[rx][ry] !='o'){
+            cpu[rx][ry]='o';
+            contador ++;
+          }
+      }while(contador < t);
 }
 
 void conta_barcos(int t){
@@ -73,35 +124,68 @@ void conta_barcos(int t){
 }
 
 void player_ataca(){
-  printf("\n[%c][%d]\n",a,b);
-  printf("Digite as Coordenadas para Atacar:");
-  scanf(" %c%d",&a,&b);
+  do{
+    printf("Digite as Coordenadas para Atacar:");
 
-  printf("\n[%d][%d]\n",a,b);
+    fgets(aa,2,stdin);
+    fgets(bb,2,stdin);
+
+    if(isalpha(aa[0]) && isdigit(bb[0])){
+      a = aa[0];
+      b = atoi(bb);
+      c = 0;
+    }
+
+    if(!( isalpha(a) && isdigit(bb[0]))){
+      printf("Você digitou Incorretamente\n");
+      c = 1;
+    }
 
 
-      if(cpu[a-96][b] == 'o'){
-        printf("Você Atirou em [%c] [%d] e Acertou\n",a-32,b);
-        cpu[a-96][b] = 'x';
-        acertos_player += 1 ;
-        barcos_cpu-=1;
-
+      if(a >= 'a' && a <= 'z'){
+        asc = 96;
+        printasc = 32;
       }
-      if(cpu[a-96][b] == '~'){
-        printf("Você Atirou em [%c] [%d] e Errou\n",a-32,b);
-        cpu[a-96][b] = 'x';
-        erros_player += 1 ;
+      if(a >= 'A' && a <= 'Z'){
+        asc = 64;
+        printasc = 0;
       }
 
-}
+      if(cpu[a-asc][b]=='x'){
+        printf("Você já Atirou Nesta Posição Ou ela é Inválida\n");
+      }
+      if(a-asc>t || b > t){
+        printf("Posição Fora do Tabuleiro\n");
+      }
+      while((aa[0] = getchar()) != '\n' && aa[0] != EOF);
+  }while(cpu[a-asc][b]=='x' || (a-asc>t || b > t) || c== 1   );
+
+        if(cpu[a-asc][b] == 'o'){
+          printf("Você Atirou em [%c] [%d] e Acertou\n",a-printasc,b);
+          cpu[a-asc][b] = 'x';
+          acertos_player += 1 ;
+          barcos_cpu-=1;
+
+        }
+        if(cpu[a-asc][b] == '~'){
+          printf("Você Atirou em [%c] [%d] e Errou\n",a-printasc,b);
+          cpu[a-asc][b] = 'x';
+          erros_player += 1 ;
+        }
+
+  }
+
+
 
 void cpu_ataca(int t){
   srand(time(NULL));
+    do{
+        ac = rand()%t+1;
+        bc = rand()%t+1;
+        printf("%d %d",ac,bc);
+    }
 
-    ac = rand()%t+1;
-    bc = rand()%t+1;
-    printf("%d %d",ac,bc);
-
+    while(player[ac][bc]=='x');
       if(player[ac][bc] == 'o'){
         printf("CPU Atirou em [%c] [%d] e Acertou\n",ac+64,bc);
         player[ac][bc] = 'x';
@@ -134,7 +218,20 @@ void desenhatab_player(int t){
       }else{printf("%3.c",player[x][y]);
       }
       if(y==t){
-      printf("\n");
+        printf("\n");
+      }
+
+    }
+}
+void desenhatabuleiro(int t){
+  for(x=0;x<=t;x++)
+    for(y=0;y<=t;y++){
+      if(player[x][y]<=t){
+        printf("%3.d",player[x][y]);
+      }else{printf("%3.c",player[x][y]);
+      }
+      if(y==t){
+        printf("\n");
       }
 
     }
@@ -160,7 +257,7 @@ void desenhatab_cpu(int t){
         printf("%3.c",cpu[x][y]);
       }
       if(y==t){
-      printf("\n");
+        printf("\n");
       }
 
     }
@@ -169,6 +266,7 @@ void gameplay(t){
 
   criaguia(t);
   criatabuleiro(t);
+  criabarco_player(t,escolher);
   criabarcoaleat(t);
   conta_barcos(t);
   desenhatab_player(t);
@@ -183,21 +281,59 @@ void gameplay(t){
     }while(barcos_player>0 && barcos_cpu>0);
 
   if(barcos_player == 0){
-    printf("Voce Perdeu !");
+    printf("Você Perdeu !");
   }
   if(barcos_cpu == 0){
-    printf("Voce Venceu !");
+    printf("Você Venceu !");
   }
 }
 main(){
+    setlocale(LC_ALL, "");
+  do{
+    if(escolher != 0){
+      printf("Valor Invalido!");
+    }
+    printf("\nVocê Deseja Escolher Suas Posições s/n ?\n");
+    escolher = getchar();
+    while( getchar() != '\n' );
+    if(escolher == 's' || escolher == 'n'){
+      ok = 1;
+    }
+    }while(ok != 1);
+  ok = 0;
+  for(x =0;x<=1;x++){
+      printf("Digite o Tamanho do Tabuleiro 5, 10, 15 ou 20 :\n");
+      scanf("%d",&t);
+      while( getchar() != '\n' );
+      switch(t){
+        case 5:
+        t = 5;
+        gameplay(t);
+        break;
 
-printf("Digite o Tamanho do Tabuleiro com no maximo 24 Posicoes:\n");
-scanf("%d",&t);
-while(t<2 && t>24){
-  printf("Digite um valor maior ou igual a 3 e menor igual a 24:\n");
-  scanf("%d",&t);
+        case 10:
+        t = 10;
+        gameplay(t);
+        break;
+
+        case 15:
+        t = 15;
+        gameplay(t);
+        break;
+
+        case 20:
+        t = 20;
+        gameplay(t);
+        break;
+
+        default :
+        x-=1;
+       printf ("Valor invalido!\n");
+      }
+
+    }
 }
-  if(t>2 && t<25){
-    gameplay(t);
-  }
-}
+
+
+
+
